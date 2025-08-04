430bf3af26ae42d2bf84e8144d8cff6e00c8d43a
jonjo <jjvanegas@innovacion.gob.sv>
2025-08-03 20:26:11 -0600

    Agrega recursos API y controladores para Categoría, Ejecución Mensual, Presupuesto, Provisión y Unidad

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\CategoriaController;
+use App\Http\Controllers\Api\EjecucionMensualController;
+use App\Http\Controllers\Api\PresupuestoController;
+use App\Http\Controllers\Api\ProvisionController;
+use App\Http\Controllers\Api\UnidadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +22,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('unidades', UnidadController::class);
+Route::apiResource('categorias', CategoriaController::class);
+Route::apiResource('presupuestos', PresupuestoController::class);
+Route::apiResource('ejecuciones', EjecucionMensualController::class);
+Route::apiResource('provisiones', ProvisionController::class);
+
+
